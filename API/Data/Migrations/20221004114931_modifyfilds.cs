e56231240203535ea3dbe5b02d8ae6dfa1b7a365
atharsk33 <athar.sk33@gmail.com>
2022-10-04 18:04:27 +0530

    End of session  & added photo gallery, added members details

---
+++ API/Data/Migrations/20221004114931_modifyfilds.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class modifyfilds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Introducation",
+                table: "Users",
+                newName: "Introduction");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Introduction",
+                table: "Users",
+                newName: "Introducation");
+        }
+    }
+}
--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { Member } from 'src/app/_models/member';
+import { MemberService } from 'src/app/_services/member.service';
+import {NgxGalleryAnimation} from '@kolkov/ngx-gallery';
+
 
 @Component({
   selector: 'app-member-details',
@@ -6,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-details.component.css']
 })
 export class MemberDetailsComponent implements OnInit {
+  member:Member;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
-  constructor() { }
+  constructor(private memberService:MemberService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation : NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ]
+  }  
+
+  getImages() : NgxGalleryImage[] {
+    const imageUrls = [];
+    for(const photo of this.member.photos ) {
+          imageUrls.push({
+            small : photo?.url,
+            medium: photo?.url,
+            big: photo?.url
+          });
+    }
+
+    return imageUrls;
+
   }
 
+  loadMember() {
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username') || '{}')
+      .subscribe(member => {
+        this.member = member;
+        this.galleryImages = this.getImages();
+      });
+    ;
+  }
 }
